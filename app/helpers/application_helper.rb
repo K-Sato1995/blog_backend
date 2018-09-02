b2c371283f0b685681d2480ba4f9e28049e21fd1
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-03 07:40:18 +0900

    markdown-function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def qiita_markdown(markdown)
+   processor = Qiita::Markdown::Processor.new(hostname: "example.com")
+   processor.call(markdown)[:output].to_s.html_safe
+ end
 end
