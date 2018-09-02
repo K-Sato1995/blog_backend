1931050460055470c4c7f217f533d8cc90ed44fb
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-02 14:48:19 +0900

    Added the new page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,27 @@
 class PostsController < ApplicationController
-
+  before_action:find_post, only: [:show]
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    if @post.save?
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :context, :status)
+  end
+
+  def find_post
+    @post = Post.find([:id])
   end
 end
