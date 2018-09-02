b0d88fd689a626d6a490edef8bce0131d96ad1ae
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-03 07:06:12 +0900

    modified model and form for the new column

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :context, :status)
+    params.require(:post).permit(:title, :context, :category, :status)
   end
 
   def find_post
