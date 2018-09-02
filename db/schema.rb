591f905cb64f9c413e33db76e14a0f6d61ce4588
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-03 07:02:27 +0900

    added category to post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180902052036) do
+ActiveRecord::Schema.define(version: 20180902220152) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180902052036) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
 end
