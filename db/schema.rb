6a6dc8aaced4b2f3ff3a586aee692644718e9c37
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-03 20:07:35 +0900

    Created user model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180902220152) do
+ActiveRecord::Schema.define(version: 20180903110440) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20180902220152) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
