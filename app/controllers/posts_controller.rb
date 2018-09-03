89e15a1c5869408fbe698aff001c5b44cec95b8a
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-03 20:48:21 +0900

    Completed setting up devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :context, :category, :status)
+    params.require(:post).permit(:title, :context, :category, :status, :user_id).merge(user_id: current_user.id)
   end
 
   def find_post
