270b88be5a6a941cb11a54b0cc09872cf7f440c1
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-03 20:11:23 +0900

    Added references to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180903110440) do
+ActiveRecord::Schema.define(version: 20180903111032) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20180903110440) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
