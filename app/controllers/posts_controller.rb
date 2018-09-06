4eb85afeaaf13536a9a8b93f2156cd92e67428f7
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-07 07:02:16 +0900

    Completed the basic setup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  before_action:authenticate_user!, only: [ :new, :edit, :update, :destroy ]
   before_action:find_post, only: [ :show, :edit, :update, :destroy ]
 
   def index
@@ -11,40 +10,7 @@ class PostsController < ApplicationController
     end
   end
 
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to root_path
-    else
-      render 'new'
-    end
-  end
-
-  def update
-    @post.update(post_params)
-    if @post.save
-      redirect_to root_path
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @post.destroy
-    redirect_to root_path
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :context, :category, :status, :image, :category_name, :user_id).merge(user_id: current_user.id)
-  end
-
-  def find_post
+  def show
     @post = Post.find(params[:id])
   end
 end
