c38e934d66904c5b2e4928b5508bb5d0dae62639
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-06 20:02:39 +0900

    Added logic to create and reference category

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,12 @@ class Post < ApplicationRecord
       all
     end
   end
+
+  def category_name=(name)
+    self.category = Category.find_or_create_by(name: name)
+  end
+
+  def category_name
+    self.category ? self.category.name: nil
+  end
 end
