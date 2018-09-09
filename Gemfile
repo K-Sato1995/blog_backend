e22e83256e3d1ac8d82bcac849f56da06af963cf
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-09 17:37:08 +0900

    Added ransack

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'markdownjs-rails'
   gem 'qiita-markdown'
+  gem 'ransack'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'web-console', '>= 3.3.0'
--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,7 +3,8 @@ class Admin::PostsController < ApplicationController
   before_action:find_post, only: [ :edit, :update, :destroy ]
 
   def index
-    @posts = Post.all
+    @q = Post.ransack(params[:q])
+    @posts = @q.result(distinct: true)
   end
 
   def new
