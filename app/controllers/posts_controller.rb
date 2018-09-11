750c89524bd396f6c1fc1944fe159a20de90d5cd
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-11 19:55:09 +0900

    added category and recent posts;

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,8 @@ class PostsController < ApplicationController
 
   def index
     @categories = Category.all
+    @recent_posts = Post.order(created_at: :DESC).limit(5)
+
     if params[:category]
       @posts = Post.published.category_search(params[:category]).page(params[:page]).per(4).order(created_at: :desc)
     else
