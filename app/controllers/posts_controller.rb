9f1f99f6845cf02190073fe121f6b396dfbc79f8
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-15 10:57:24 +0900

    Pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,9 @@ class PostsController < ApplicationController
     @recent_posts = Post.order(created_at: :DESC).limit(5)
 
     if params[:category]
-      @posts = Post.published.category_search(params[:category]).page(params[:page]).per(4).order(created_at: :desc)
+      @posts = Post.published.category_search(params[:category]).page(params[:page]).per(12).order(created_at: :desc)
     else
-      @posts = Post.published.search(params[:title]).page(params[:page]).per(5).order(created_at: :desc)
+      @posts = Post.published.search(params[:title]).page(params[:page]).per(12).order(created_at: :desc)
     end
   end
 
