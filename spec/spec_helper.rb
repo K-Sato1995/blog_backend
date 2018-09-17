e306ca184bdd9fe3ece2422410028705aaee6952
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-17 10:49:24 +0900

    ready to deploy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,9 +12,9 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+require 'devise'
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  require 'devise'
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
