36d5a4e26761864b64cafd60d1439d6bd1b2fdd4
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-17 10:58:32 +0900

    ready to deploy

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,5 +1,5 @@
 //Show the preview of markdown text
 function setPreview(val){
-  let preview = document.getElementById('preview');
+  var preview = document.getElementById('preview');
   preview.innerHTML = html_content = markdown.toHTML(val);
 }
