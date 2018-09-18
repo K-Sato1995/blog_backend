c33523c1b3450b86953451bf2d89d910893abf83
Katsuki <Katsuki_Sato0130@icloud.com>
2018-09-19 07:23:18 +0900

    commented out preview feature

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,5 +1,5 @@
 //Show the preview of markdown text
-function setPreview(val){
-  var preview = document.getElementById('preview');
-  preview.innerHTML = html_content = markdown.toHTML(val);
-}
+// function setPreview(val){
+//   var preview = document.getElementById('preview');
+//   preview.innerHTML = html_content = markdown.toHTML(val);
+// }
