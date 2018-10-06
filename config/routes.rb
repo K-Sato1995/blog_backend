ab5d8fc8fab3aa41bbce371573dcf859b6f8872f
Katsuki <Katsuki_Sato0130@icloud.com>
2018-10-06 22:17:43 +0900

    added archive

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: { sign_up: '' }
   root 'posts#index'
+  get '/archives', to: 'posts#archive'
   resources :posts, only: [:index, :show]
   namespace :admin do
     resources :posts
