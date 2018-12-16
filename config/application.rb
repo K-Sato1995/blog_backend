7f880f328ca867a7836a6d4669e021af1455df78
Katsuki <Katsuki_Sato0130@icloud.com>
2018-12-16 15:44:13 +0900

    completed i18n for static strings

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+I18n.available_locales = %i(en ja)
+I18n.default_locale = :en
 
 module MyBlog
   class Application < Rails::Application
