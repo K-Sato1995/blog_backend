c0a54f3e6e5ad66da42d991dc1de0c40ba27a2e4
Katsuki <Katsuki_Sato0130@icloud.com>
2018-12-18 19:45:10 +0900

    uninstall rails-markdown-js

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,5 +1,1 @@
-//Show the preview of markdown text
-// function setPreview(val){
-//   var preview = document.getElementById('preview');
-//   preview.innerHTML = html_content = markdown.toHTML(val);
-// }
+
