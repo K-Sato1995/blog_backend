f29dfbf7eb6b2e26ceec628038bfe39c19d46b23
佐藤克樹 <k-sato@ruby-dev.jp>
2018-12-27 09:14:23 +0900

    Chnage static attributes to dynamic in factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name 'name'
-    email 'email@example.com'
-    password '000000'
-    password_confirmation '000000'
+    name { 'name' }
+    email { 'email@example.com' }
+    password { '000000' }
+    password_confirmation { '000000' }
   end
 end
