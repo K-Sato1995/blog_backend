025da8d74bc6b6d75db193c661caeca1ee17acdf
佐藤克樹 <k-sato@ruby-dev.jp>
2018-12-27 09:53:28 +0900

    Add some feature tests

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,10 +3,10 @@ FactoryBot.define do
     title { 'Title' }
     context { 'This is the context of this post' }
     status { 1 }
-    image { 'image_string' }
+    image { 'https://images.pexels.com/photos/954202/pexels-photo-954202.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' }
     user { build(:user) }
     score { 1 }
-    
+
     association :category, factory: :category, name: 'Ruby'
   end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,15 +2,46 @@ require 'rails_helper'
 
 feature 'Posts', type: :feature do
   describe 'The order of posts' do
-    let(:post1) { build(:post, title: 'post_2019_12_01', score: 5, created_at: Time.new(2019, 12, 01)) }
-    let(:post2) { build(:post, title: 'post_2019_12_02', score: 5, created_at: Time.new(2019, 12, 02)) }
-    let(:post3) { build(:post, title: 'post_2019_12_03', score: 2, created_at: Time.new(2019, 12, 03)) }
+    before(:all) do
+      create(:post, title: 'post_2019_12_01', score: 5, created_at: Time.new(2019, 12, 01), category: create(:category, name: 'JS'))
+      create(:post, title: 'post_2019_12_02', score: 5, created_at: Time.new(2019, 12, 02))
+      create(:post, title: 'post_2019_12_03', score: 2, created_at: Time.new(2019, 12, 03))
+    end
+
+    after(:all) do
+      Post.delete_all
+    end
 
     it 'should be sorted based on the score of each post. If multiple posts have the same score, those posts should be sorted dscendingly by created date of each post' do
       visit '/en'
       expect(all('.box')[0]).to have_content('post_2019_12_02')
       expect(all('.box')[1]).to have_content('post_2019_12_01')
       expect(all('.box')[2]).to have_content('post_2019_12_03')
+    end
+  end
+
+  describe 'Selected Category' do
+    before(:all) do
+      create(:post, category: create(:category, name: 'JS'))
+      create(:post)
+      create(:post)
+    end
+
+    before(:each) do
+      visit '/en'
+      click_link 'JS'
+    end
+
+    after(:all) do
+      Post.delete_all
+    end
+
+    it 'should only show posts with the selected cateogory' do
+      expect(all('.box').size).to eq(1)
+    end
+
+    it 'should have the selected category' do
+      expect(all('.box')[0]).to have_content('JS')
     end
   end
 end
