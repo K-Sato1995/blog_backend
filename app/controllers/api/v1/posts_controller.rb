a56810476b685a3eec67d49d9089f07419ad530b
Katsuki <Katsuki_Sato0130@icloud.com>
2019-01-26 22:02:09 +0900

    Modify the tag part of API

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,7 +3,7 @@ module Api
     class PostsController < ApplicationController
       def index
         posts = Post.published.order(score: :desc, created_at: :desc)
-        post_tags = Post.order(score: :desc, created_at: :desc).map { |post| Post.find_by(id: post.id).tags }
+        post_tags = Post.published.order(score: :desc, created_at: :desc).map { |post| Post.find_by(id: post.id).tags }
         categories = Category.all
         tags = Tag.all.map { |tag| Tag.find_by(name: tag.name).posts }
         render json: { status: 'SUCCESS', message: 'loaded posts', data: { posts: posts, categories: categories, tags: tags, post_tags: post_tags } }
