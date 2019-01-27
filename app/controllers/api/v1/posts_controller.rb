3350609e15e7069fc95ecf59d14845016a422ac0
Katsuki <Katsuki_Sato0130@icloud.com>
2019-01-27 16:29:09 +0900

    Make api able to pass tags

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,7 +5,7 @@ module Api
         posts = Post.published.order(score: :desc, created_at: :desc)
         post_tags = Post.published.order(score: :desc, created_at: :desc).map { |post| Post.find_by(id: post.id).tags }
         categories = Category.all
-        tags = Tag.all.map { |tag| Tag.find_by(name: tag.name).posts }
+        tags = Tag.all
         render json: { status: 'SUCCESS', message: 'loaded posts', data: { posts: posts, categories: categories, tags: tags, post_tags: post_tags } }
       end
 
