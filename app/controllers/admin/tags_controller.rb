fb821ab7d0cb2aee55884c7fe74088dd62025344
Katsuki <Katsuki_Sato0130@icloud.com>
2019-05-21 21:35:59 +0900

    Add tag UI

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,5 +1,34 @@
-class TagsController < ApplicationController
+module Admin
+  class TagsController < ApplicationController
+    before_action:find_tag, only: [:edit, :update, :destroy]
 
-  def index
+    def index
+      @tags = Tag.all
+    end
+
+    def update
+      if @tag.update(tag_params)
+        flash[:warning] = 'The tag was successfully updated'
+        redirect_to admin_tags_path
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @tag.destroy
+      flash[:danger] = 'The tag was successfully deleted'
+      redirect_to admin_tags_path
+    end
+
+    private
+
+    def tag_params
+      params.require(:tag).permit(:name)
+    end
+
+    def find_tag
+      @tag = Tag.find(params[:id])
+    end
   end
 end
