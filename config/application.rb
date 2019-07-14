5a4997db5e7d03b6bd341b0807143e3211883d4b
Katsuki <Katsuki_Sato0130@icloud.com>
2019-07-15 02:56:41 +0900

    Fix Cors

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module MyBlog
   class Application < Rails::Application
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:8000', 'k-sato1995.github.io/MyBlogFront/'
+        origins 'localhost:8000', 'k-sato1995.github.io'
         resource '*', headers: :any, methods: [:get, :put, :post]
       end
     end
