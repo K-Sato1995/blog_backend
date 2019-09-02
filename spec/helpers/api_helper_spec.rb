331ae786ef8b85d3de7ba8db9f4abb0a87387f28
Katsuki <Katsuki_Sato0130@icloud.com>
2019-09-03 06:12:46 +0900

    Fix rspec

--- spec/helpers/api_helper_spec.rb
+++ spec/helpers/api_helper_spec.rb
@@ -5,12 +5,22 @@ RSpec.describe ApiHelper, type: :helper do
   let(:ins) { K.new }
 
   describe 'Api helper methods' do
-    it 'show_attributes' do
-      expect(ins.show_attributes).to eq(%w[id title content status created_at updated_at user_id category_id score page_views introduction language like slug])
+    describe 'show_attributes' do
+      it 'only shows speficfied attributes' do
+        expect(ins.show_attributes).to eq(%w[id title content status created_at updated_at user_id category_id score page_views introduction language like slug])
+      end
     end
 
-    it 'index_attributes' do
-      expect(ins.index_attributes).to eq(%w[id title status created_at updated_at category_id score introduction language like slug])
+    describe 'index_attributes' do
+      it 'only shows speficfied attributes' do
+        expect(ins.index_attributes).to eq(%w[id title category_id score introduction language like slug])
+      end
+    end
+
+    describe 'featured_posts_attributes' do
+      it 'only shows speficfied attributes' do
+        expect(ins.featured_posts_attributes).to eq(%w[id title slug])
+      end
     end
   end
 end
--- spec/requests/api/v1/posts_controller_spec.rb
+++ spec/requests/api/v1/posts_controller_spec.rb
@@ -45,7 +45,7 @@ describe 'API::V1::PostsController' do
     end
 
     it 'sends a list of posts' do
-      expect(json['data'].length).to eq(5)
+      expect(json['data']['posts'].length).to eq(5)
     end
   end
 
