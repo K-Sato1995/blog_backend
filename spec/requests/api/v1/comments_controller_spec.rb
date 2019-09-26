bc0442f7d5d2a064b21eacbca95489425cb4a7de
Katsuki <Katsuki_Sato0130@icloud.com>
2019-09-26 21:16:44 +0900

    Add different context to comments test && Refactor by using subject

--- spec/requests/api/v1/comments_controller_spec.rb
+++ spec/requests/api/v1/comments_controller_spec.rb
@@ -1,16 +1,28 @@
 require 'rails_helper'
 
-describe 'API::V1::CommentsController' do
+describe 'API::V2::CommentsController' do
   let(:user) { create(:user) }
 
   describe 'POST /api/v1/posts/:post_id/comments' do
     let(:json) { JSON.parse(response.body) }
     let(:post1) { create(:post) }
+    let(:params) { { name: 'name', content: 'content' } }
+    subject { post "/api/v1/posts/#{post1.slug}/comments", params: { comment: params } }
 
-    it 'creates a comment' do
-      valid_params = { name: 'name', content: 'content' }
-      expect { post "/api/v1/posts/#{post1.slug}/comments", params: { comment: valid_params } }.to change(Comment, :count).by(+1)
-      expect(response.status).to eq(200)
+    context 'when the valid values are passed' do
+      it 'creates a comment' do
+        expect { subject }.to change(Comment, :count).by(+1)
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'When the passed values are invalid' do
+      let(:params) { { name: '', content: '' } }
+
+      it 'returns an error' do
+        expect { subject }.to change(Comment, :count).by(0)
+        expect(json['message']).to eq('An error occured')
+      end
     end
   end
 end
--- spec/requests/api/v2/comments_controller_spec.rb
+++ spec/requests/api/v2/comments_controller_spec.rb
@@ -6,25 +6,23 @@ describe 'API::V2::CommentsController' do
   describe 'POST /api/v2/posts/:post_id/comments' do
     let(:json) { JSON.parse(response.body) }
     let(:post1) { create(:post) }
+    let(:params) { { name: 'name', content: 'content' } }
+    subject { post "/api/v2/posts/#{post1.slug}/comments", params: { comment: params } }
 
     context 'when the valid values are passed' do
-      valid_params = { name: 'name', content: 'content' }
-
       it 'creates a comment' do
-        expect { post "/api/v2/posts/#{post1.slug}/comments", params: { comment: valid_params } }.to change(Comment, :count).by(+1)
+        expect { subject }.to change(Comment, :count).by(+1)
         expect(response.status).to eq(200)
       end
     end
 
-    # context 'When the passed values are invalid' do
-    #   let!(:invalid_params) do
-    #     {
-    #       comment: { name: '', content: '' }
-    #     }
-    #   end
-    #   it 'returns an error' do
-    #     expect { post "/api/v2/posts/#{post1.slug}/comments", params: invalid_params }.to change(Comment, :count).by(+1)
-    #   end
-    # end
+    context 'When the passed values are invalid' do
+      let(:params) { { name: '', content: '' } }
+
+      it 'returns an error' do
+        expect { subject }.to change(Comment, :count).by(0)
+        expect(json['message']).to eq('An error occured')
+      end
+    end
   end
 end
