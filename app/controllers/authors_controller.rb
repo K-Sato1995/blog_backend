9bcbe051ad85f2fe16e83df92e59c5bda250310b
Katsuki <Katsuki_Sato0130@icloud.com>
2019-10-02 12:01:15 +0900

    Add authorization

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,6 @@
 class AuthorsController < ApplicationController
+  before_action :authenticate_author
+
   def show
     @author = Author.find(params[:id])
   end
@@ -21,5 +23,17 @@ class AuthorsController < ApplicationController
 
   def author_params
     params.require(:author).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def authenticate_author
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_author
+    @author = Author.find(params[:id])
+    redirect_to(root_url) unless current_author?(@author)
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,10 @@ module SessionsHelper
     session[:author_id] = author.id
   end
 
+  def current_author?(author)
+    author == current_author
+  end
+
   def current_author
     if (author_id = session[:author_id])
       @current_author ||= User.find_by(id: author_id)
